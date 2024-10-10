42e394c0a3ebce3604a921033edb6e3e150a5c9b
Tatshu02 <lucia.F.M182@gmail.com>
2024-10-10 19:01:39 -0300

    Add files via upload

--- boton.js
+++ boton.js
@@ -1,17 +1,11 @@
-function Botoncircular(label, x, y, diam) {
-  push();
-  translate(x, y);
-  fill('#894444'); // Cambié el color a formato hexadecimal
-  noStroke();
-  ellipse(0, 0, diam, diam);
-  fill(255);
+// Función para crear botones rectangulares
+function botonRectangular(texto, x, y, ancho, alto) {
+  rect(x, y, ancho, alto);
   textAlign(CENTER, CENTER);
-  textSize(20);
-  text(label, 0, 0);
-  pop();
+  text(texto, x + ancho / 2, y + alto / 2);
 }
 
-function superficie(x, y, xSup, ySup, rSup) {
-  let distancia = dist(x, y, xSup, ySup);
-  return distancia < rSup; // Devuelve true o false directamente
+// Función para verificar si el mouse está sobre un botón
+function superficie(mx, my, x, y, ancho, alto) {
+  return mx > x && mx < x + ancho && my > y && my < y + alto;
 }
